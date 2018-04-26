cf080fec18ad7b321491db0aa89fbd52e5c7c257
root <root@GC173417.local.ajinomoto.com>
2018-04-26 18:26:19 +0900

    googlemapを表示

---
+++ src/components/MyMap.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mymap">
+    <GmapMap
+    :center="{lat: 35.40, lng: 139.46}"
+    :zoom="10"
+    style="width: 500px; height: 300px"
+    >
+    <GmapMarker
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+      :clickable="false"
+      :draggable="false"
+      @click="center=m.position"
+    />
+    </GmapMap>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mymap',
+  data () {
+    return {
+      markers: [
+        { position: { lat: 35.40, lng: 139.46 } },
+        { position: { lat: 35.41, lng: 139.56 } }
+      ]
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import {API_KEY_GOOGLE_MAP} from './constants/api_key'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+/**
+ * googleMaplibraryのload
+ */
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: API_KEY_GOOGLE_MAP,
+    libraries: 'places'
+  }
+})
+
 new Vue({
   el: '#app',
   router,
